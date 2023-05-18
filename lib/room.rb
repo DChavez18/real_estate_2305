406fc1b1d57784676d51403eb0a4f72aeb928a57
Derek Chavez <frrenose17@yahoo.com>
2023-05-18 09:33:52 -0600

    feat: Add area method and instance variables

--- lib/room.rb
+++ lib/room.rb
@@ -1,6 +1,18 @@
 class Room
 
-  def initialize
+  attr_reader :category, :length, :width
+  def initialize(category, length, width)
+    @category = category
+    @length = length
+    @width = width
+    @is_painted = false
 
   end
+
+  def area
+    length * width.to_i
+  end
+
+
+
 end
