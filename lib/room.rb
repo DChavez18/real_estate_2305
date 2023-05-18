9c838e868612470b29d0561eebfd1e7a04d4324a
Derek Chavez <frrenose17@yahoo.com>
2023-05-18 09:39:53 -0600

    feat: Add is_painted? method

--- lib/room.rb
+++ lib/room.rb
@@ -1,17 +1,23 @@
 class Room
 
-  attr_reader :category, :length, :width
+  attr_reader :category, :length, :width, :is_painted
   def initialize(category, length, width)
     @category = category
     @length = length
     @width = width
-    @is_painted = false
+    @is_painted = is_painted
 
   end
 
   def area
     length * width.to_i
   end
+  
+  def is_painted?
+    false
+  end
+
+
 
 
 
